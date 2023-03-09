5aaa71b8bdfda62588ff584d355ff0ebcfdbf1ce
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-09 18:10:15 +0200

    feat: addtime

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export const add = (a: number, b: number): number => a + b
+export function addTime(date: Date, time: number, type: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months' | 'years') {
+	switch (type) {
+		case 'seconds':
+			return new Date(date.getTime() + time * 1000)
+		case 'minutes':
+			return new Date(date.getTime() + time * 1000 * 60)
+		case 'hours':
+			return new Date(date.getTime() + time * 1000 * 60 * 60)
+		case 'days':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24)
+		case 'weeks':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 7)
+		case 'months':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 30)
+		case 'years':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 365)
+		default:
+			throw new Error('Invalid type')
+	}
+}
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,14 @@
-import { describe, it, expect } from 'vitest'
-import { add } from '../src'
+import { describe, expect, it } from 'vitest'
+import { addTime } from '../src'
 
 describe('test', () => {
 	it('test', () => {
-		expect(add(1, 2)).toBe(3)
+		expect(addTime(new Date(2021, 0, 1), 1, 'seconds')).toStrictEqual(new Date(2021, 0, 1, 0, 0, 1))
+		expect(addTime(new Date(2021, 0, 1), 1, 'minutes')).toStrictEqual(new Date(2021, 0, 1, 0, 1))
+		expect(addTime(new Date(2021, 0, 1), 1, 'hours')).toStrictEqual(new Date(2021, 0, 1, 1))
+		expect(addTime(new Date(2021, 0, 1), 1, 'days')).toStrictEqual(new Date(2021, 0, 2))
+		expect(addTime(new Date(2021, 0, 1), 1, 'weeks')).toStrictEqual(new Date(2021, 0, 8))
+		expect(addTime(new Date(2021, 3, 1), 1, 'months')).toStrictEqual(new Date(2021, 4, 1))
+		expect(addTime(new Date(2021, 0, 1), 1, 'years')).toStrictEqual(new Date(2022, 0, 1))
 	})
 })
