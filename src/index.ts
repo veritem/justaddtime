264091969492b1351f1be7caa3c3ac2066b49235
Makuza Mugabo Verite <mugaboverite@gmail.com>
2023-03-12 17:27:01 +0200

    chore: updated

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,36 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable no-case-declarations */
 export default function addTime(date: Date, time: number, type: 'seconds' | 'minutes' | 'hours' | 'days' | 'weeks' | 'months' | 'years') {
+	if (typeof date !== 'object') throw new Error('Invalid date')
 
-  if (typeof date !== 'object') throw new Error('Invalid date')
+	switch (type) {
+		case 'seconds':
+			return new Date(date.getTime() + time * 1000)
+		case 'minutes':
+			return new Date(date.getTime() + time * 1000 * 60)
+		case 'hours':
+			return new Date(date.getTime() + time * 1000 * 60 * 60)
+		case 'days':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24)
+		case 'weeks':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 7)
+		case 'months':
 
-  switch (type) {
-    case 'seconds':
-      return new Date(date.getTime() + time * 1000)
-    case 'minutes':
-      return new Date(date.getTime() + time * 1000 * 60)
-    case 'hours':
-      return new Date(date.getTime() + time * 1000 * 60 * 60)
-    case 'days':
-      return new Date(date.getTime() + time * 1000 * 60 * 60 * 24)
-    case 'weeks':
-      return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 7)
-    case 'months':
+			const months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
+			const months_with_30_days = [4, 6, 9, 11]
 
-      const months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
-      const months_with_30_days = [4, 6, 9, 11]
+			const month = date.getMonth() + 1
 
-      const month = date.getMonth() + 1
+			if (months_with_31_days.includes(month))
+				return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 31)
+			else if (months_with_30_days.includes(month))
+				return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 30)
+			else
+				return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 28)
 
-      if (months_with_31_days.includes(month)) {
-        return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 31)
-      } else if (months_with_30_days.includes(month)) {
-        return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 30)
-      } else {
-        return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 28)
-      }
-
-    case 'years':
-      return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 365)
-    default:
-      throw new Error('Invalid type')
-  }
+		case 'years':
+			return new Date(date.getTime() + time * 1000 * 60 * 60 * 24 * 365)
+		default:
+			throw new Error('Invalid type')
+	}
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,13 +2,16 @@ import { describe, expect, it } from 'vitest'
 import justaddtime from '../src'
 
 describe('test', () => {
-  it('test', () => {
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'seconds')).toStrictEqual(new Date(2021, 0, 1, 0, 0, 1))
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'minutes')).toStrictEqual(new Date(2021, 0, 1, 0, 1))
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'hours')).toStrictEqual(new Date(2021, 0, 1, 1))
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'days')).toStrictEqual(new Date(2021, 0, 2))
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'weeks')).toStrictEqual(new Date(2021, 0, 8))
-    expect(justaddtime(new Date(2021, 3, 1), 1, 'months')).toStrictEqual(new Date(2021, 4, 1))
-    expect(justaddtime(new Date(2021, 0, 1), 1, 'years')).toStrictEqual(new Date(2022, 0, 1))
-  })
+	it('test', () => {
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'seconds')).toStrictEqual(new Date(2021, 0, 1, 0, 0, 1))
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'minutes')).toStrictEqual(new Date(2021, 0, 1, 0, 1))
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'hours')).toStrictEqual(new Date(2021, 0, 1, 1))
+	})
+
+	it('test', () => {
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'days')).toStrictEqual(new Date(2021, 0, 2))
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'weeks')).toStrictEqual(new Date(2021, 0, 8))
+		expect(justaddtime(new Date(2021, 3, 1), 1, 'months')).toStrictEqual(new Date(2021, 4, 1))
+		expect(justaddtime(new Date(2021, 0, 1), 1, 'years')).toStrictEqual(new Date(2022, 0, 1))
+	})
 })
